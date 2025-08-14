6f0665569bc9480b9445991365970a58c1c178bc
GabrielF4 <112709330+GabrielF4@users.noreply.github.com>
2025-08-14 21:57:03 +0200

    Added tab functionality and restructured the project.

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import "./App.css";
 import PriceCalculator from "./priceCalculator/PriceCalculator";
 import BrandSearch from "./brandSearch/BrandSearch";
 
 function App() {
+    const [activeTab, setActiveTab] = useState("brandSearch");
+
+    const renderTabContent = () => {
+        switch (activeTab) {
+            case "brandSearch":
+                return <BrandSearch />;
+            case "priceCalc":
+                return <PriceCalculator />;
+            default:
+                return <BrandSearch />;
+        }
+    };
+
     return (
-        <div className="appContainer">
-            <div className="brandSearchContainer">
-                <BrandSearch />
-            </div>
-            <div className="priceCalcContainer">
-                <PriceCalculator />
-            </div>
-        </div>
+        <>
+            <nav className="nav-bar">
+                <div
+                    className="tab"
+                    onClick={() => setActiveTab("brandSearch")}
+                >
+                    Märkessökning
+                </div>
+                <div className="tab" onClick={() => setActiveTab("priceCalc")}>
+                    Prissättning
+                </div>
+            </nav>
+            <div className="appContainer">{renderTabContent()}</div>
+        </>
     );
 }
 
