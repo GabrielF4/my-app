86664ef426ee97665e432cb179b15cf460268fe1
GabrielF4 <112709330+GabrielF4@users.noreply.github.com>
2025-08-17 19:03:33 +0200

    Restructured app.jsx and its css files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,8 @@
-import { useState, useEffect } from "react";
 import "./App.css";
-import PriceCalculator from "./priceCalculator/PriceCalculator";
-import BrandSearch from "./brandSearch/BrandSearch";
+import MainPage from "./MainPage";
 import logo from "../assets/logo.png";
 
 function App() {
-    //State for active tab
-    const [activeTab, setActiveTab] = useState(() => {
-        return localStorage.getItem("activeTab") || "brandSearch";
-    });
-
-    //Get active tab from local storage on load
-    useEffect(() => {
-        localStorage.setItem("activeTab", activeTab);
-    }, [activeTab]);
-
-    //Render content based on the activeTab state
-    const renderTabContent = () => {
-        switch (activeTab) {
-            case "brandSearch":
-                return <BrandSearch />;
-            case "priceCalc":
-                return <PriceCalculator />;
-            default:
-                return <BrandSearch />;
-        }
-    };
-
     return (
         <>
             <header>
@@ -37,23 +13,7 @@ function App() {
                 />
             </header>
             <main>
-                <>
-                    <nav className="nav-bar">
-                        <div
-                            className="tab"
-                            onClick={() => setActiveTab("brandSearch")}
-                        >
-                            Märkessökning
-                        </div>
-                        <div
-                            className="tab"
-                            onClick={() => setActiveTab("priceCalc")}
-                        >
-                            Prissättning
-                        </div>
-                    </nav>
-                    <div className="appContainer">{renderTabContent()}</div>
-                </>
+                <MainPage />
             </main>
             <footer>
                 Obs! This is a private project and isn't made by the red cross
