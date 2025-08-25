d64a05cafe1b25e64c68fd8f76b4fd2582a956cb
GabrielF4 <112709330+GabrielF4@users.noreply.github.com>
2025-08-25 19:40:59 +0200

    Added color to the week based on what to color to mark the prices numbers in store

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,33 @@ import MainPage from "./pages/MainPage";
 import logo from "./assets/logo.png";
 
 function getWeekNumber(d = new Date()) {
+    return 28;
     return Math.ceil(
         ((d - new Date(d.getFullYear(), 0, 1)) / 86400000 +
             new Date(d.getFullYear(), 0, 1).getDay() +
             1) /
             7
     );
+}
+
+function getWeekColor(week) {
+    const colors = new Map([
+        [0, "#334093ff"],
+        [1, "#4E8C4C"],
+        [2, "#C44545"],
+        [3, "#1c1c1cff"],
+        [4, "#fff700ff"],
+    ]);
+
+    //Week 28 is missing
+    return week < 28 ? colors.get(week % 5) : colors.get((week + 1) % 5);
+}
+
+function getWeekTextColor(week) {
+    let thisWeek = week < 28 ? week : week + 1;
+
+    //Week 28 is missing
+    return thisWeek % 5 === 4 ? "black" : "white";
 }
 
 function App() {
@@ -22,7 +43,15 @@ function App() {
                         alt="Svenska Röda Korset Logotyp"
                         className="logo"
                     />
-                    <div className="weekLabel">Vecka: {getWeekNumber()}</div>
+                    <div
+                        className="weekLabel"
+                        style={{
+                            backgroundColor: getWeekColor(getWeekNumber()),
+                            color: getWeekTextColor(getWeekNumber()),
+                        }}
+                    >
+                        Vecka: {getWeekNumber()}
+                    </div>
                 </header>
                 <main>
                     <MainPage />
